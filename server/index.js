37090e2efc59baa20777e7a1b03698bd509d3e45
UBnkantah <ubongnkantahuwem@gmail.com>
2024-01-21 21:24:13 +0000

    update

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const payRoute = require('./routes/PayRoute');
 const app = express()
 const port = 8000
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+
 app.use(cors())
 app.use(express.json());
 app.use(express.static("public"));
